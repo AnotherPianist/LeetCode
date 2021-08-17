d2ebc5fd77f53c0036726795904d5ea094226907
AnotherPianist <32588448+AnotherPianist@users.noreply.github.com>
2021-08-17 18:57:12 -0400

    Time: 28 ms (83.71%), Space: 14.3 MB (42.15%) - LeetHub

--- fibonacci-number/fibonacci-number.py
+++ fibonacci-number/fibonacci-number.py
@@ -1,11 +1,6 @@
 class Solution:
     def fib(self, n: int) -> int:
-        m = {0: 0, 1: 1}
-        
-        def f(n):
-            if n in m:
-                return m[n]
-            m[n] = f(n - 1) + f(n - 2)
-            return m[n]
-        
-        return f(n)
+        prev, current = 0, 1
+        for _ in range(n):
+            prev, current = current, prev + current
+        return prev
