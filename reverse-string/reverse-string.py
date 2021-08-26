e9118f28c4601260809599cfdf00293d5b09d67c
AnotherPianist <32588448+AnotherPianist@users.noreply.github.com>
2021-08-25 22:18:19 -0400

    Time: 409 ms (5.06%), Space: 18.4 MB (81.91%) - LeetHub

--- reverse-string/reverse-string.py
+++ reverse-string/reverse-string.py
@@ -3,4 +3,8 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        s.reverse()
+        i, j = 0, len(s) - 1
+        while i < j:
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
