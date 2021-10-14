77c4f7a45d0ad63a9c58efb746e02988f896c13c
AnotherPianist <32588448+AnotherPianist@users.noreply.github.com>
2021-10-14 01:14:52 -0300

    Time: 2805 ms (59.45%), Space: 14.2 MB (86.66%) - LeetHub

---
+++ perfect-squares/perfect-squares.py
@@ -0,0 +1,9 @@
+from math import sqrt
+
+
+class Solution:
+    def numSquares(self, n: int) -> int:
+        dp = [0]
+        while len(dp) <= n:
+            dp.append(min(dp[-i * i] for i in range(1, int(sqrt(len(dp))) + 1)) + 1)
+        return dp[n]
