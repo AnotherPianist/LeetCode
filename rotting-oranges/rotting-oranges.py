a8b44de6c294c8f69871b98f91afe21d24c84194
AnotherPianist <32588448+AnotherPianist@users.noreply.github.com>
2021-10-28 22:14:42 -0300

    Time: 52 ms (79.23%), Space: 14.4 MB (39.39%) - LeetHub

--- rotting-oranges/rotting-oranges.py
+++ rotting-oranges/rotting-oranges.py
@@ -1,12 +1,13 @@
+from collections import deque
+
+
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         if not grid:
             return -1
 
         q = deque()
-        coordinates = ((-1, 0), (0, 1), (1, 0), (0, -1))
-        time = 0
-        fresh_count = 0
+        time = fresh_count = 0
 
         for i, row in enumerate(grid):
             for j, val in enumerate(row):
@@ -18,13 +19,12 @@ class Solution:
         while q and fresh_count > 0:
             for _ in range(len(q)):
                 i, j = q.popleft()
-                for x, y in coordinates:
+                for x, y in ((-1, 0), (0, 1), (1, 0), (0, -1)):
                     adj_i, adj_j = i + x, j + y
-                    if adj_i < 0 or adj_i == len(grid) or adj_j < 0 or adj_j == len(grid[0]) or grid[adj_i][adj_j] == 0 or grid[adj_i][adj_j] == 2:
-                        continue
-                    grid[adj_i][adj_j] = 2
-                    q.append((adj_i, adj_j))
-                    fresh_count -= 1
+                    if 0 <= adj_i < len(grid) and 0 <= adj_j < len(grid[0]) and grid[adj_i][adj_j] == 1:
+                        grid[adj_i][adj_j] = 2
+                        q.append((adj_i, adj_j))
+                        fresh_count -= 1
             time += 1
 
         if fresh_count > 0:
