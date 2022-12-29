a077123a820504abbd6b30fa8bb0b65f645633d7
Carlos Daniel Ríos Moya <32588448+AnotherPianist@users.noreply.github.com>
2022-12-29 03:20:19 -0300

    Time: 786 ms (70.52%), Space: 20.1 MB (74.53%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.py
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -1,30 +1,32 @@
+from collections import deque, defaultdict
+
+
 class Solution:
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        neighbors = defaultdict(list)
+        color = [None] * (n + 1)
+        
         def bfs(source):
             q = deque([source])
-            color[source] = 0 # Start with marking source as 'RED'
+            color[source] = 0
+            
             while q:
                 node = q.popleft()
-                for neighbor in adj[node]:
-                    # If there is a conflict, return false.
-                    if color[neighbor] == color[node]: return False
-                    if color[neighbor] == -1:
-                        color[neighbor] = 1 - color[node]
+                for neighbor in neighbors[node]:
+                    if color[neighbor] == color[node]:
+                        return False
+                    if not color[neighbor]:
+                        color[neighbor] = 0 if color[node] == 1 else 1
                         q.append(neighbor)
-            
             return True
         
-        adj = [[] for _ in range(n + 1)]
         for dislike in dislikes:
-            adj[dislike[0]].append(dislike[1])
-            adj[dislike[1]].append(dislike[0])
+            neighbors[dislike[0]].append(dislike[1])
+            neighbors[dislike[1]].append(dislike[0])
         
-        color = [-1] * (n + 1) # 0 stands for red and 1 stands for blue.
         for i in range(1, n + 1):
-            if color[i] == -1:
-                # For each pending component, run BFS.
+            if not color[i]:
                 if not bfs(i):
-                    # Return false, if there is conflict in the component.
                     return False
         
         return True
