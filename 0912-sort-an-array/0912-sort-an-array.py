e5c70fa00ecf5c550b58d1fdf9ae8387b9d2f331
Carlos Daniel Ríos Moya <32588448+AnotherPianist@users.noreply.github.com>
2023-03-01 20:07:02 -0300

    Time: 2678 ms (18.81%), Space: 33.2 MB (7.17%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,47 +1,79 @@
-class Solution:
-    def sortArray(self, nums: List[int]) -> List[int]:
-        
 #################################################################
         #                   Merge Sort                  #
 
-        temp_arr = [0] * len(nums)
+# class Solution:
+#     def sortArray(self, nums: List[int]) -> List[int]:
+
+#         temp_arr = [0] * len(nums)
         
-        def merge(left, mid, right):
-            for i in range(mid - left + 1):
-                temp_arr[left + i] = nums[left + i]
-            for i in range(right - mid):
-                temp_arr[mid + 1 + i] = nums[mid + 1 + i]
+#         def merge(left, mid, right):
+#             for i in range(mid - left + 1):
+#                 temp_arr[left + i] = nums[left + i]
+#             for i in range(right - mid):
+#                 temp_arr[mid + 1 + i] = nums[mid + 1 + i]
                 
-            i, j, k = 0, 0, left
+#             i, j, k = 0, 0, left
             
-            while i < mid - left + 1 and j < right - mid:
-                if temp_arr[left + i] <= temp_arr[mid + 1 + j]:
-                    nums[k] = temp_arr[left + i]
-                    i += 1
-                else:
-                    nums[k] = temp_arr[mid + 1 + j]
-                    j += 1
-                k += 1
+#             while i < mid - left + 1 and j < right - mid:
+#                 if temp_arr[left + i] <= temp_arr[mid + 1 + j]:
+#                     nums[k] = temp_arr[left + i]
+#                     i += 1
+#                 else:
+#                     nums[k] = temp_arr[mid + 1 + j]
+#                     j += 1
+#                 k += 1
             
-            while i < mid - left + 1:
-                nums[k] = temp_arr[left + i]
-                i += 1
-                k += 1
-            while j < right - mid:
-                nums[k] = temp_arr[mid + 1 + j]
-                j += 1
-                k += 1
+#             while i < mid - left + 1:
+#                 nums[k] = temp_arr[left + i]
+#                 i += 1
+#                 k += 1
+#             while j < right - mid:
+#                 nums[k] = temp_arr[mid + 1 + j]
+#                 j += 1
+#                 k += 1
         
-        def merge_sort(left, right):
-            if left >= right:
-                return
+#         def merge_sort(left, right):
+#             if left >= right:
+#                 return
             
-            mid = left + (right - left) // 2
+#             mid = left + (right - left) // 2
             
-            merge_sort(left, mid)
-            merge_sort(mid + 1, right)
-            merge(left, mid, right)
+#             merge_sort(left, mid)
+#             merge_sort(mid + 1, right)
+#             merge(left, mid, right)
         
-        merge_sort(0, len(nums) - 1)
+#         merge_sort(0, len(nums) - 1)
         
+#         return nums
+    
+
+
+#################################################################
+        #                   Heap Sort                  #
+    
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        
+        def heapify(n, i):  # top-down
+            largest = i
+            left, right = 2 * i + 1, 2 * i + 2
+            
+            if left < n and nums[left] > nums[largest]:
+                largest = left
+            if right < n and nums[right] > nums[largest]:
+                largest = right
+            
+            if largest != i:
+                nums[i], nums[largest] = nums[largest], nums[i]
+                heapify(n, largest)
+        
+        def heap_sort():
+            for i in range(len(nums) // 2 - 1, -1, -1):
+                heapify(len(nums), i)
+            
+            for i in range(len(nums) - 1, -1, -1):
+                nums[0], nums[i] = nums[i], nums[0]
+                heapify(i, 0)
+        
+        heap_sort()
         return nums
