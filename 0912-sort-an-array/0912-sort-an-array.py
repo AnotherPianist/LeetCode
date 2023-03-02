ba3ab7eef602cacc90121d409c6bc4489977d988
Carlos Daniel Ríos Moya <32588448+AnotherPianist@users.noreply.github.com>
2023-03-02 01:43:07 -0300

    Time: 895 ms (84.20%), Space: 27.1 MB (13.90%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -51,29 +51,57 @@
 #################################################################
         #                   Heap Sort                  #
     
+# class Solution:
+#     def sortArray(self, nums: List[int]) -> List[int]:
+        
+#         def heapify(n, i):  # top-down
+#             largest = i
+#             left, right = 2 * i + 1, 2 * i + 2
+            
+#             if left < n and nums[left] > nums[largest]:
+#                 largest = left
+#             if right < n and nums[right] > nums[largest]:
+#                 largest = right
+            
+#             if largest != i:
+#                 nums[i], nums[largest] = nums[largest], nums[i]
+#                 heapify(n, largest)
+        
+#         def heap_sort():
+#             for i in range(len(nums) // 2 - 1, -1, -1):
+#                 heapify(len(nums), i)
+            
+#             for i in range(len(nums) - 1, -1, -1):
+#                 nums[0], nums[i] = nums[i], nums[0]
+#                 heapify(i, 0)
+        
+#         heap_sort()
+#         return nums
+
+
+
+#################################################################
+        #                 Counting Sort                  #
+
+from collections import defaultdict
+
+
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
+        count = defaultdict(int)
         
-        def heapify(n, i):  # top-down
-            largest = i
-            left, right = 2 * i + 1, 2 * i + 2
-            
-            if left < n and nums[left] > nums[largest]:
-                largest = left
-            if right < n and nums[right] > nums[largest]:
-                largest = right
-            
-            if largest != i:
-                nums[i], nums[largest] = nums[largest], nums[i]
-                heapify(n, largest)
+        min_val, max_val = float("inf"), float("-inf")
         
-        def heap_sort():
-            for i in range(len(nums) // 2 - 1, -1, -1):
-                heapify(len(nums), i)
-            
-            for i in range(len(nums) - 1, -1, -1):
-                nums[0], nums[i] = nums[i], nums[0]
-                heapify(i, 0)
+        for num in nums:
+            min_val = min(min_val, num)
+            max_val = max(max_val, num)
+            count[num] += 1
         
-        heap_sort()
+        i = 0
+        for num in range(min_val, max_val + 1, 1):
+            while count[num]:
+                nums[i] = num
+                i += 1
+                count[num] -= 1
+        
         return nums
