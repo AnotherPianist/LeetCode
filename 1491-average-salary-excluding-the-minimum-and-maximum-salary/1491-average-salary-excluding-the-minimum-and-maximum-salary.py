b85e941c50c6eba409485cb7d04684a4abbb9e01
Carlos Daniel Ríos Moya <32588448+AnotherPianist@users.noreply.github.com>
2023-05-01 15:19:29 -0400

    Time: 56 ms (5.49%), Space: 16.3 MB (8.21%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
@@ -2,8 +2,10 @@ class Solution:
     def average(self, salary: List[int]) -> float:
         min_val, max_val = float("inf"), float("-inf")
         sum = 0
+
         for i, val in enumerate(salary):
             min_val = min(min_val, val)
             max_val = max(max_val, val)
             sum += val
+
         return (sum - min_val - max_val) / (i - 1)
