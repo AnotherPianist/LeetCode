a1cd73763f7e3ee1753dab8449ab890fb1e1cd09
Carlos Daniel Ríos Moya <32588448+AnotherPianist@users.noreply.github.com>
2023-05-07 17:28:01 -0400

    Time: 64 ms (70.30%), Space: 44.7 MB (50.15%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,22 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let currentCount = init;
+    
+    const increment = () => ++currentCount;
+    
+    const decrement = () => --currentCount;
+    
+    const reset = () => currentCount = init;
+    
+    return { increment, decrement, reset }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
