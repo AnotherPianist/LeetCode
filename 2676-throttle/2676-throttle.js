81b880848753acf7427d73383221af14c7e4e405
Carlos Daniel Ríos Moya <32588448+AnotherPianist@users.noreply.github.com>
2023-05-20 19:04:15 -0400

    Time: 73 ms (8.15%), Space: 42.1 MB (50.00%) - LeetHub

---
+++ 2676-throttle/2676-throttle.js
@@ -0,0 +1,34 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var throttle = function(fn, t) {
+  let intervalInProgress = null;
+  let argsToProcess = null;
+  
+  function intervalFunction() {
+    if (argsToProcess === null) {
+      clearInterval(intervalInProgress);
+      intervalInProgress = null;
+    } else {
+      fn(...argsToProcess);
+      argsToProcess = null;
+    }
+  }
+  
+  return function(...args) {
+    if (intervalInProgress)
+      argsToProcess = args;
+    else {
+      fn(...args);
+      intervalInProgress = setInterval(intervalFunction, t);
+    }
+  }
+};
+
+/**
+ * const throttled = throttle(console.log, 100);
+ * throttled("log"); // logged immediately.
+ * throttled("log"); // logged at t=100ms.
+ */
