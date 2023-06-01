6bade2e0aa91c5454861a9a27ed99b653f9c6d88
Carlos Daniel Ríos Moya <32588448+AnotherPianist@users.noreply.github.com>
2023-05-31 20:26:06 -0400

    Time: 268 ms (12.72%), Space: 27.7 MB (18.12%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.py
+++ 1396-design-underground-system/1396-design-underground-system.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 class UndergroundSystem:
 
     def __init__(self):
-        self.times = defaultdict(list)
+        self.times = defaultdict(lambda: (0, 0))
         self.start_times = {}
         
 
@@ -14,13 +14,14 @@ class UndergroundSystem:
 
     def checkOut(self, id: int, stationName: str, t: int) -> None:
         start_station, start_time = self.start_times[id]
-        self.times[(start_station, stationName)].append(t - start_time)
+        prev_time, prev_count = self.times[(start_station, stationName)]
+        self.times[(start_station, stationName)] = (prev_time + t - start_time, prev_count + 1)
         
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
-        times = self.times[(startStation, endStation)]
+        time, count = self.times[(startStation, endStation)]
         
-        return sum(times) / len(times)
+        return time / count
         
 
 
