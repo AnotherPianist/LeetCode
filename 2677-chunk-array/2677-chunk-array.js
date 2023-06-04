39e73d01db53c00bae732ebf7d132876f41b5830
Carlos Daniel Ríos Moya <32588448+AnotherPianist@users.noreply.github.com>
2023-06-04 17:56:07 -0400

    Time: 66 ms (44.70%), Space: 44.8 MB (32.86%) - LeetHub

---
+++ 2677-chunk-array/2677-chunk-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array[]}
+ */
+var chunk = function(arr, size) {
+    const result = [];
+    let i = 0;
+    
+    while (i < arr.length) {
+        result.push(arr.slice(i, i + size));
+        i += size;
+    }
+    
+    return result;
+};
