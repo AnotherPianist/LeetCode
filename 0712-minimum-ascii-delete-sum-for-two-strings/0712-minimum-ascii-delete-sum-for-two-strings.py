bb68650ec18663c7c81718e9606de097dbb1b401
Carlos Daniel Ríos Moya <32588448+AnotherPianist@users.noreply.github.com>
2023-07-31 19:52:31 -0400

    Time: 446 ms (94.92%), Space: 16.4 MB (91.20%) - LeetHub

---
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        if len(s1) < len(s2):
+            return self.minimumDeleteSum(s1=s2, s2=s1)
+        
+        m, n = len(s1), len(s2)
+        curr_row = [0] * (n + 1)
+        for j in range(1, n + 1):
+            curr_row[j] = curr_row[j - 1] + ord(s2[j - 1])
+        
+        for i in range(1, m + 1):
+            diag = curr_row[0]
+            curr_row[0] += ord(s1[i - 1])
+
+            for j in range(1, n + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    answer = diag
+                else:
+                    answer = min(ord(s1[i - 1]) + curr_row[j], ord(s2[j - 1]) + curr_row[j - 1])
+
+                diag = curr_row[j]
+                curr_row[j] = answer
+
+        return curr_row[-1]
